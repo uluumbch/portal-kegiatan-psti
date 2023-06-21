4c3070656cb2ab1a29ffef717292ba2b0099ef89
Bachrul Uluum <uluum123@gmail.com>
2023-06-21 16:04:56 +0800

    menambahkan akunseeder

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -14,7 +14,11 @@
             <div class=" overflow-hidden shadow-sm sm:rounded-lg">
                 {{-- show alert --}}
                 @if(session()->has('status'))
-                <div class="alert alert-success">
+                <div class="alert alert-success"
+                    x-data="{ show: true }"
+                    x-show="show"
+                    x-transition
+                    x-init="setTimeout(() => show = false, 3000)">
                     <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                     <span>{{ session('status') }}</span>
                   </div>
